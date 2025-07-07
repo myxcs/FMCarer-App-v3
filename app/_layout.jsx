83c9c845ec3a0fa390ee8c98d5a8f5d9391c2bdb
myxcs <118356147+myxcs@users.noreply.github.com>
2025-07-07 16:03:11 +0700

    login ui

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,4 +1,5 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeScreen from "../components/SafeScreen";
 
@@ -7,10 +8,11 @@ export default function RootLayout() {
     <SafeAreaProvider>
       <SafeScreen>
         <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="(auth)" />
-      </Stack>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(auth)" />
+        </Stack>
       </SafeScreen>
+      <StatusBar style="dark" />
     </SafeAreaProvider>
   );
 }
